74844b4dd3b936bcca1e3135dcf80af7c7de06a1
j-miet <191247923+j-miet@users.noreply.github.com>
2026-02-18 17:54:58 +0200

    Add main program

--- PixelRay/Program.cs
+++ PixelRay/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using PixelRay.Mathematics;
+
+static class CreatePixelRay
+{
+    public static void Main(string[] args)
+    {
+        Vec3 v = new();
+        Console.WriteLine(v);
+    }
+}
+

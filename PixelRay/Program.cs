2bc306b5ccb5ff24dc14780cd81b5b1bcbbb9edf
j-miet <191247923+j-miet@users.noreply.github.com>
2026-02-19 15:33:44 +0200

    Update main

--- PixelRay/Program.cs
+++ PixelRay/Program.cs
@@ -1,11 +1,32 @@
-﻿using PixelRay.Mathematics;
+﻿using PixelRay.Core;
+using PixelRay.Geometry;
+using PixelRay.Mathematics;
+using PixelRay.Ouput;
+using PixelRay.SceneObjects;
 
 static class CreatePixelRay
 {
     public static void Main(string[] args)
     {
-        Vec3 v = new();
-        Console.WriteLine(v);
+        const int WIDTH = 160;
+        const int HEIGHT = 120;
+
+        Scene scene = new();
+
+        scene.Objects.Add(new Sphere(
+            new Vec3(0, 0, -2),
+            1,
+            new ColorRGB(1, 0.2, 0.2)
+        ));
+
+        Camera camera = new(
+            new Vec3(0, 0, 0),
+            WIDTH,
+            HEIGHT
+        );
+        Renderer renderer = new(WIDTH, HEIGHT, 4, new Vec3(-1, -1, -1));
+        FrameBuffer buffer = renderer.Render(scene, camera);
+        ImageWriter.WritePPM("output.ppm", buffer);
     }
 }
 

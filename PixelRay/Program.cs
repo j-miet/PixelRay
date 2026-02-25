6417acf48a1aae31447bff0e3ebd9efe41337062
j-miet <191247923+j-miet@users.noreply.github.com>
2026-02-25 23:54:20 +0200

    Test triangle shape

--- PixelRay/Program.cs
+++ PixelRay/Program.cs
@@ -20,26 +20,32 @@ static class CreatePixelRay
         Scene scene = new();
 
         scene.Objects.Add(new Sphere(
-            new Vec3(0, 0, -2),
-            1,
+            new Vec3(-1, 0.25, -2),
+            0.5,
             new ColorRGB(1, 0.2, 0.2)
         ));
 
         scene.Objects.Add(new Plane(
-            new Vec3(0, -1, 0),
+            new Vec3(0, -0.25, 0),
             new Vec3(0, 1, 0),
             new ColorRGB(0.1, 0.8, 0.8)
         ));
+
+
+        scene.Objects.Add(new Triangle(
+            new Vec3(0.2, -0.25, -0.5),
+            new Vec3(0.8, -0.25, -1),
+            new Vec3(0.4, 0.25, -0.75),
+            new ColorRGB(0.3, 0.7, 0.5)
+        ));
+
 
         scene.Lights.Add(new DirectionalLight(
             new Vec3(-1, -1, -1),
             new ColorRGB(1, 1, 1)
         ));
 
-        scene.Lights.Add(new DirectionalLight(
-            new Vec3(1, -1, -1),
-            new ColorRGB(0.2, 0.5, 0.7)
-        ));
+
 
 
         Camera camera = new(

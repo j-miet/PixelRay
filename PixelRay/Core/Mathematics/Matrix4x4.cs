890886ecd67f3d2fe694c74031f256fea4146071
j-miet <191247923+j-miet@users.noreply.github.com>
2026-03-08 18:07:14 +0200

    Fix errors, add aliases and improve inverse calculations

--- PixelRay/Core/Mathematics/Matrix4x4.cs
+++ PixelRay/Core/Mathematics/Matrix4x4.cs
@@ -53,17 +53,15 @@ public readonly struct Matrix4x4(
     /// </summary>
     public Ray TransformRay(Ray r)
     {
-        return new Ray(
-            TransformPoint(r.Origin),
-            TransformVector(r.Direction)
-        );
+        Vec3 O = TransformPoint(r.Origin);
+        Vec3 D = TransformVector(r.Direction);
+
+        return new Ray(O, D);
     }
 
     /// <summary>
-    /// Matrix m inverse for affine transforms. These include translation, rotation, scaling and any combination
-    /// of these. 
-    /// Upper-left 3x3 rotation matrix must be orthonormal. If not, use general Inverse method instead which will be
-    /// slower.
+    /// Matrix m inverse for affine transforms. These include translation, rotation, scaling (including non-uniform) 
+    /// and any combination of these.
     /// </summary>
     public Matrix4x4 InverseAffine()
     {
@@ -75,20 +73,38 @@ public readonly struct Matrix4x4(
         // [ R^-1 -(R^1)*T]
         // [ 0   0   0  1 ]
         // Furthermore: because R is a rotation matrix thus orthonormal, its inverse is equal to transpose.
-        Matrix4x4 rInv = new( // R^-1 = R^T
-            M00, M10, M20, 0,
-            M01, M11, M21, 0,
-            M02, M12, M22, 0,
-            0, 0, 0, 1
-        );
+        // However this version cannot be applied to scaling transforms because orthonormal condition breaks. Therefore
+        // orthonormal assumption cannot be used, but final inverse has same structure as stated above
+        double determinant = M00 * (M11 * M22 - M12 * M21) - M01 * (M10 * M22 - M12 * M20)
+                            + M02 * (M10 * M21 - M11 * M20);
 
-        Vec3 t = new(M03, M13, M23);
-        Vec3 tInverse = -rInv.TransformVector(t); // -(R^1)*T
+        if (Math.Abs(determinant) < 1e-12)
+            throw new InvalidOperationException("Matrix not invertible");
+
+        double invDet = 1.0 / determinant;
+
+        // A^-1 = (1 / det) * Adj(A) where Adj(A) is the adjugate matrix.
+        double r00 = (M11 * M22 - M12 * M21) * invDet;
+        double r01 = (M02 * M21 - M01 * M22) * invDet;
+        double r02 = (M01 * M12 - M02 * M11) * invDet;
+
+        double r10 = (M12 * M20 - M10 * M22) * invDet;
+        double r11 = (M00 * M22 - M02 * M20) * invDet;
+        double r12 = (M02 * M10 - M00 * M12) * invDet;
+
+        double r20 = (M10 * M21 - M11 * M20) * invDet;
+        double r21 = (M01 * M20 - M00 * M21) * invDet;
+        double r22 = (M00 * M11 - M01 * M10) * invDet;
+
+        // -(A⁻¹ * t); this applies transform properly so coordinate x becomes x - a after applying transform vector a
+        double t0 = -(r00 * M03 + r01 * M13 + r02 * M23);
+        double t1 = -(r10 * M03 + r11 * M13 + r12 * M23);
+        double t2 = -(r20 * M03 + r21 * M13 + r22 * M23);
 
         return new Matrix4x4(
-            rInv.M00, rInv.M01, rInv.M02, tInverse.X,
-            rInv.M10, rInv.M11, rInv.M12, tInverse.Y,
-            rInv.M20, rInv.M21, rInv.M22, tInverse.Z,
+            r00, r01, r02, t0,
+            r10, r11, r12, t1,
+            r20, r21, r22, t2,
             0, 0, 0, 1
         );
     }
@@ -99,79 +115,49 @@ public readonly struct Matrix4x4(
     /// <exception cref="InvalidOperationException"></exception>
     public Matrix4x4 Inverse()
     {
-        double[] m =
-        [
-            M00,M01,M02,M03,
-            M10,M11,M12,M13,
-            M20,M21,M22,M23,
-            M30,M31,M32,M33
-        ];
+        double b00 = M00 * M11 - M01 * M10;
+        double b01 = M00 * M12 - M02 * M10;
+        double b02 = M00 * M13 - M03 * M10;
+        double b03 = M01 * M12 - M02 * M11;
+        double b04 = M01 * M13 - M03 * M11;
+        double b05 = M02 * M13 - M03 * M12;
+        double b06 = M20 * M31 - M21 * M30;
+        double b07 = M20 * M32 - M22 * M30;
+        double b08 = M20 * M33 - M23 * M30;
+        double b09 = M21 * M32 - M22 * M31;
+        double b10 = M21 * M33 - M23 * M31;
+        double b11 = M22 * M33 - M23 * M32;
 
-        double[] dets = new double[16]; // determinants of all inner 2x2 matrices
+        double det =
+            b00 * b11 - b01 * b10 + b02 * b09 +
+            b03 * b08 - b04 * b07 + b05 * b06;
 
-        dets[0] = M11 * M22 * M33 - M11 * M23 * M32 - M21 * M12 * M33
-               + M21 * M13 * M32 + M31 * M12 * M23 - M31 * M13 * M22;
+        if (Math.Abs(det) < 1e-12)
+            throw new InvalidOperationException("Matrix not invertible");
 
-        dets[4] = -M10 * M22 * M33 + M10 * M23 * M32 + M20 * M12 * M33
-               - M20 * M13 * M32 - M30 * M12 * M23 + M30 * M13 * M22;
+        double invDet = 1.0 / det;
 
-        dets[8] = M10 * M21 * M33 - M10 * M23 * M31 - M20 * M11 * M33
-               + M20 * M13 * M31 + M30 * M11 * M23 - M30 * M13 * M21;
-
-        dets[12] = -M10 * M21 * M32 + M10 * M22 * M31 + M20 * M11 * M32
-                - M20 * M12 * M31 - M30 * M11 * M22 + M30 * M12 * M21;
-
-        dets[1] = -M01 * M22 * M33 + M01 * M23 * M32 + M21 * M02 * M33
-               - M21 * M03 * M32 - M31 * M02 * M23 + M31 * M03 * M22;
-
-        dets[5] = M00 * M22 * M33 - M00 * M23 * M32 - M20 * M02 * M33
-               + M20 * M03 * M32 + M30 * M02 * M23 - M30 * M03 * M22;
-
-        dets[9] = -M00 * M21 * M33 + M00 * M23 * M31 + M20 * M01 * M33
-               - M20 * M03 * M31 - M30 * M01 * M23 + M30 * M03 * M21;
-
-        dets[13] = M00 * M21 * M32 - M00 * M22 * M31 - M20 * M01 * M32
-                + M20 * M02 * M31 + M30 * M01 * M22 - M30 * M02 * M21;
-
-        dets[2] = M01 * M12 * M33 - M01 * M13 * M32 - M11 * M02 * M33
-               + M11 * M03 * M32 + M31 * M02 * M13 - M31 * M03 * M12;
-
-        dets[6] = -M00 * M12 * M33 + M00 * M13 * M32 + M10 * M02 * M33
-               - M10 * M03 * M32 - M30 * M02 * M13 + M30 * M03 * M12;
-
-        dets[10] = M00 * M11 * M33 - M00 * M13 * M31 - M10 * M01 * M33
-                + M10 * M03 * M31 + M30 * M01 * M13 - M30 * M03 * M11;
-
-        dets[14] = -M00 * M11 * M32 + M00 * M12 * M31 + M10 * M01 * M32
-                - M10 * M02 * M31 - M30 * M01 * M12 + M30 * M02 * M11;
-
-        dets[3] = -M01 * M12 * M23 + M01 * M13 * M22 + M11 * M02 * M23
-               - M11 * M03 * M22 - M21 * M02 * M13 + M21 * M03 * M12;
-
-        dets[7] = M00 * M12 * M23 - M00 * M13 * M22 - M10 * M02 * M23
-               + M10 * M03 * M22 + M20 * M02 * M13 - M20 * M03 * M12;
-
-        dets[11] = -M00 * M11 * M23 + M00 * M13 * M21 + M10 * M01 * M23
-                - M10 * M03 * M21 - M20 * M01 * M13 + M20 * M03 * M11;
-
-        dets[15] = M00 * M11 * M22 - M00 * M12 * M21 - M10 * M01 * M22
-                + M10 * M02 * M21 + M20 * M01 * M12 - M20 * M02 * M11;
-
-        double determinant = M00 * dets[0] + M01 * dets[4] + M02 * dets[8] + M03 * dets[12];
-
-        if (Math.Abs(determinant) < 1e-12)
-            throw new InvalidOperationException("Matrix not detsertible");
-
-        double inverseDet = 1.0 / determinant;
-
-        for (int i = 0; i < 16; i++)
-            dets[i] *= inverseDet;
-
+        // final inverse by direct calculation using shared subdeterminants b_ij
         return new Matrix4x4(
-            dets[0], dets[1], dets[2], dets[3],
-            dets[4], dets[5], dets[6], dets[7],
-            dets[8], dets[9], dets[10], dets[11],
-            dets[12], dets[13], dets[14], dets[15]
+            (M11 * b11 - M12 * b10 + M13 * b09) * invDet,
+            (-M01 * b11 + M02 * b10 - M03 * b09) * invDet,
+            (M31 * b05 - M32 * b04 + M33 * b03) * invDet,
+            (-M21 * b05 + M22 * b04 - M23 * b03) * invDet,
+
+            (-M10 * b11 + M12 * b08 - M13 * b07) * invDet,
+            (M00 * b11 - M02 * b08 + M03 * b07) * invDet,
+            (-M30 * b05 + M32 * b02 - M33 * b01) * invDet,
+            (M20 * b05 - M22 * b02 + M23 * b01) * invDet,
+
+            (M10 * b10 - M11 * b08 + M13 * b06) * invDet,
+            (-M00 * b10 + M01 * b08 - M03 * b06) * invDet,
+            (M30 * b04 - M31 * b02 + M33 * b00) * invDet,
+            (-M20 * b04 + M21 * b02 - M23 * b00) * invDet,
+
+            (-M10 * b09 + M11 * b07 - M12 * b06) * invDet,
+            (M00 * b09 - M01 * b07 + M02 * b06) * invDet,
+            (-M30 * b03 + M31 * b01 - M32 * b00) * invDet,
+            (M20 * b03 - M21 * b01 + M22 * b00) * invDet
         );
     }
 
@@ -214,20 +200,20 @@ public readonly struct Matrix4x4(
         );
     }
 
-    /// <summary>
-    /// Translation/shifting matrix to direction v
-    /// </summary>
-    public static Matrix4x4 Shift(Vec3 v) => Translate(v); // alias for Translate
+    // translate aliases
+    public static Matrix4x4 Translate(double x, double y, double z) => Translate(new Vec3(x, y, z));
+    public static Matrix4x4 Shift(Vec3 v) => Translate(v);
+    public static Matrix4x4 Shift(double x, double y, double z) => Translate(new Vec3(x, y, z));
 
     /// <summary>
-    /// Scaling matrix using vector v components as scalars
+    /// Scaling matrix to direction (x, y, z)
     /// </summary>
-    public static Matrix4x4 Scale(Vec3 v)
+    public static Matrix4x4 Scale(double x, double y, double z)
     {
         return new Matrix4x4(
-            v.X, 0, 0, 0,
-            0, v.Y, 0, 0,
-            0, 0, v.Z, 0,
+            x, 0, 0, 0,
+            0, y, 0, 0,
+            0, 0, z, 0,
             0, 0, 0, 1
         );
     }
@@ -271,7 +257,7 @@ public readonly struct Matrix4x4(
     public static Matrix4x4 operator *(Matrix4x4 m1, Matrix4x4 m2)
     {
         return new Matrix4x4(
-            m1.M00 * m1.M00 + m1.M01 * m2.M10 + m1.M02 * m2.M20 + m1.M03 * m2.M30,
+            m1.M00 * m2.M00 + m1.M01 * m2.M10 + m1.M02 * m2.M20 + m1.M03 * m2.M30,
             m1.M00 * m2.M01 + m1.M01 * m2.M11 + m1.M02 * m2.M21 + m1.M03 * m2.M31,
             m1.M00 * m2.M02 + m1.M01 * m2.M12 + m1.M02 * m2.M22 + m1.M03 * m2.M32,
             m1.M00 * m2.M03 + m1.M01 * m2.M13 + m1.M02 * m2.M23 + m1.M03 * m2.M33,

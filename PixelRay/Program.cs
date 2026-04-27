0aefb11c182cee898998c368d31aa5913fd1f986
j-miet <191247923+j-miet@users.noreply.github.com>
2026-04-27 15:28:39 +0300

    Update test environment to use materials

--- PixelRay/Program.cs
+++ PixelRay/Program.cs
@@ -1,7 +1,5 @@
 ﻿using PixelRay.Core;
-using PixelRay.Core.Mathematics;
-using PixelRay.SceneView.Scene;
-using PixelRay.SceneView.Camera;
+using PixelRay.SceneView;
 using PixelRay.SceneView.HitObjects;
 using PixelRay.SceneView.Lighting;
 using PixelRay.Output;
@@ -10,6 +8,7 @@ using PixelRay.SceneView.Hittable;
 
 using DebugMode = PixelRay.Const.DebugMode;
 using M4 = PixelRay.Core.Mathematics.Matrix4x4;
+using PixelRay.SceneView.Materials;
 
 static class CreatePixelRay
 {
@@ -19,17 +18,19 @@ static class CreatePixelRay
         const int HEIGHT = 140;
         const string outputPPM = "output.ppm";
 
-        // renderer
+        // renderer init
         Palette colorPalette = new([]);
         const int lightingBands = 32;
         const int ambientFactor = 0;
+        const int maxDepth = 1; // reflection bounces; keep at 1
 
+        // render settings
         const DebugMode debugMode = DebugMode.None;
         const int upScaleFactor = 3;
 
         // scene: camera and scene objects
         Camera camera = new(
-            new Vec3(0, 0, 0),
+            new(0, 0, 0),
             WIDTH,
             HEIGHT,
             2,
@@ -37,75 +38,136 @@ static class CreatePixelRay
         );
         Scene scene = new();
 
+        // objects
         scene.AddObject(new Transform(
-            new Sphere(new ColorRGB(0.7, 0.2, 0.2)),
+            new Sphere(
+                new FlatMaterial(
+                    new(0.7, 0.2, 0.2),
+                    0
+                )
+            ),
             M4.Translate(-0.25, -0.15, -0.25) *
             M4.Scale(0.05)
         ));
 
-        scene.AddObject(new Triangle(
-            new Vec3(0.2, -0.25, -0.3),
-            new Vec3(1.1, -0.25, -0.8),
-            new Vec3(0.9, 0.25, -0.65),
-            new ColorRGB(0.8, 0.9, 0.1)
-        ));
+        scene.AddObject(
+            new Triangle(
+                new(0.2, -0.25, -0.3),
+                new(1.1, -0.25, -0.8),
+                new(0.9, 0.25, -0.65),
+                new FlatMaterial(
+                    new(0.8, 0.9, 0.1),
+                    0
+                )
+            )
+        );
 
         scene.AddObject(new Transform(
-            new Cylinder(new ColorRGB(0.1, 0.3, 0.9)),
+            new Cylinder(
+                new FlatMaterial(
+                    new(0.1, 0.3, 0.9),
+                    0.5
+                )
+            ),
             M4.Translate(-0.6, -0.25, -0.75) *
             M4.Scale(0.25, 0.35, 0.25)
         ));
 
         scene.AddObject(new Transform(
-            new Cone(new ColorRGB(0.8, 0.1, 0.1)),
+            new Cone(
+                new FlatMaterial(
+                    new(0.8, 0.1, 0.1),
+                    0
+                )
+            ),
             M4.Translate(-0.6, 0.45, -0.75) *
-            M4.Rotate(new Vec3(1, 0, 0), Math.PI) *
+            M4.Rotate(new(1, 0, 0), Math.PI) *
             M4.Scale(0.25, 0.35, 0.25)
         ));
 
         scene.AddObject(new Transform(
-            new Disc(new ColorRGB(0.8, 0.2, 0.8)),
-            M4.Translate(-1.5, 0.65, -1.2) *
-            M4.Rotate(new Vec3(0, 1, 0), -Math.PI / 32) *
-            M4.Rotate(new Vec3(1, 0, 0), Math.PI / 2) *
+            new Disc(
+                new FlatMaterial(
+                    new(0.8, 0.2, 0.8),
+                    0
+                )
+            ),
+            M4.Translate(-1.55, 0.7, -1.2) *
+            M4.Rotate(new(0, 1, 0), -Math.PI / 32) *
+            M4.Rotate(new(1, 0, 0), Math.PI / 2) *
             M4.Scale(0.25, 1.0, 0.25)
         ));
 
         scene.AddObject(new Transform(
-            new Torus(new ColorRGB(0.5, 1.0, 0.5), 0.2, 0.1),
+            new Torus(
+                0.2,
+                0.1,
+                new FlatMaterial(
+                    new(0.5, 1.0, 0.5),
+                    0
+                )
+            ),
             M4.Translate(0, 0.1, -0.75) *
-            M4.Rotate(new Vec3(1, 0, 0), Math.PI / 2)
+            M4.Rotate(new(1, 0, 0), Math.PI / 2)
         ));
 
         scene.AddObject(new Transform(
-            new Plane(new ColorRGB(0.1, 0.8, 0.8)),
+            new Plane(
+                new FlatMaterial(
+                    new(0.1, 0.8, 0.8),
+                    0
+                )
+            ),
             M4.Translate(0, -0.25, 0)
         ));
 
         scene.AddObject(new Transform(
+            new Plane(
+                new FlatMaterial(
+                    new(1, 1, 1),
+                    0
+                )
+            ),
+            M4.Translate(0, 0, -20) *
+            M4.Rotate(new(1, 0, 0), Math.PI / 2)
+        ));
+
+        scene.AddObject(new Transform(
             new Quadric(
-                new ColorRGB(0.5, 0.5, 0.5),
                 1.0, -1.0, 1.0,
                 0, 0, 0,
                 0, 0, 0,
                 -0.2,
-                xmin: -3,
-                xmax: 3,
-                ymin: -3,
-                ymax: 3,
-                zmin: -3,
-                zmax: 3
+                new(-3, -3, -3),
+                new(3, 3, 3),
+                new FlatMaterial(
+                    new(0.5, 0.5, 0.5),
+                    0
+                )
             ),
-            M4.Translate(new Vec3(2, 0.5, -3))
+            M4.Translate(new(2, 0.5, -3))
         ));
 
+        scene.AddObject(new Transform(
+            new AABox(
+                new(-0.05, -0.05, -0.05),
+                new(0.05, 0.05, 0.05),
+                new FlatMaterial(
+                    new(1, 0.65, 0),
+                    0
+                )
+            ),
+            M4.Translate(new(0.14, -0.1, -0.30))
+        ));
+
+        // lights
         scene.AddLight(new DirectionalLight(
-            new Vec3(0, -0.3, -1),
-            new ColorRGB(1, 1, 1)
+            new(0.2, -0.3, -1),
+            new(1, 1, 1)
         ));
 
         // render, save output image and display it
-        Renderer renderer = new(WIDTH, HEIGHT, colorPalette, lightingBands, ambientFactor);
+        Renderer renderer = new(WIDTH, HEIGHT, colorPalette, lightingBands, ambientFactor, maxDepth);
         FrameBuffer buffer = renderer.Render(scene, camera, upScaleFactor, debugMode);
         ImageWriter.WritePPM(outputPPM, buffer);
 

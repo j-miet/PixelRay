af65aa7f9dc7a972e4deb63db57750878689e5fe
j-miet <191247923+j-miet@users.noreply.github.com>
2026-04-30 15:40:56 +0300

    Update main program

--- PixelRay/Program.cs
+++ PixelRay/Program.cs
@@ -1,14 +1,15 @@
 ﻿using PixelRay.Core;
-using PixelRay.SceneView;
-using PixelRay.SceneView.HitObjects;
-using PixelRay.SceneView.Lighting;
+using PixelRay.Core.Mathematics;
 using PixelRay.Output;
 using PixelRay.Rendering;
+using PixelRay.SceneView;
+using PixelRay.SceneView.HitObjects;
 using PixelRay.SceneView.Hittable;
+using PixelRay.SceneView.Lighting;
+using PixelRay.SceneView.Materials;
 
 using DebugMode = PixelRay.Const.DebugMode;
 using M4 = PixelRay.Core.Mathematics.Matrix4x4;
-using PixelRay.SceneView.Materials;
 
 static class CreatePixelRay
 {
@@ -18,11 +19,20 @@ static class CreatePixelRay
         const int HEIGHT = 140;
         const string outputPPM = "output.ppm";
 
+        // camera
+        Vec3 cameraOrigin = new(0, 0, 0);
+        Vec3 lookAt = new(0, 0, -1);
+        Vec3 upDirection = new(0, 1, 0);
+        double fov = 90;
+        double aspectRatio = (double)(WIDTH) / HEIGHT;
+
         // renderer init
         Palette colorPalette = new([]);
         const int lightingBands = 32;
         const int ambientFactor = 0;
         const int maxDepth = 1; // reflection bounces; keep at 1
+        const bool useDithering = true;
+        const double ditherStrength = 0.1;
 
         // render settings
         const DebugMode debugMode = DebugMode.None;
@@ -30,11 +40,11 @@ static class CreatePixelRay
 
         // scene: camera and scene objects
         Camera camera = new(
-            new(0, 0, 0),
-            WIDTH,
-            HEIGHT,
-            2,
-            1
+            cameraOrigin,
+            lookAt,
+            upDirection,
+            fov,
+            aspectRatio
         );
         Scene scene = new();
 
@@ -46,7 +56,7 @@ static class CreatePixelRay
                     0
                 )
             ),
-            M4.Translate(-0.25, -0.15, -0.25) *
+            M4.Translate(-0.3, -0.2, -0.28) *
             M4.Scale(0.05)
         ));
 
@@ -112,27 +122,6 @@ static class CreatePixelRay
         ));
 
         scene.AddObject(new Transform(
-            new Plane(
-                new FlatMaterial(
-                    new(0.1, 0.8, 0.8),
-                    0
-                )
-            ),
-            M4.Translate(0, -0.25, 0)
-        ));
-
-        scene.AddObject(new Transform(
-            new Plane(
-                new FlatMaterial(
-                    new(1, 1, 1),
-                    0
-                )
-            ),
-            M4.Translate(0, 0, -20) *
-            M4.Rotate(new(1, 0, 0), Math.PI / 2)
-        ));
-
-        scene.AddObject(new Transform(
             new Quadric(
                 1.0, -1.0, 1.0,
                 0, 0, 0,
@@ -157,17 +146,60 @@ static class CreatePixelRay
                     0
                 )
             ),
-            M4.Translate(new(0.14, -0.1, -0.30))
+            M4.Translate(new(0.22, -0.2, -0.45))
+        ));
+
+        // floor
+        scene.AddObject(new Transform(
+            new Plane(
+                new FlatMaterial(
+                    new(0.1, 0.8, 0.8),
+                    0
+                )
+            ),
+            M4.Translate(0, -0.25, 0)
+        ));
+
+        // background
+        scene.AddObject(new Transform(
+            new Plane(
+                new FlatMaterial(
+                    new(1, 1, 1),
+                    0
+                )
+            ),
+            M4.Translate(0, 0, -20) *
+            M4.Rotate(new(1, 0, 0), Math.PI / 2)
         ));
 
         // lights
+        scene.AddLight(new AmbientLight(
+            new(1, 1, 1),
+            0.05
+        ));
+
         scene.AddLight(new DirectionalLight(
-            new(0.2, -0.3, -1),
-            new(1, 1, 1)
+           new(-0.2, 0.5, 1),
+           new(0.1, 0.1, 0.1)
+        ));
+
+        scene.AddLight(new PointLight(
+            new(0, 0.5, -0.05),
+            new(0.5, 0.5, 0.5),
+            1.5
         ));
 
         // render, save output image and display it
-        Renderer renderer = new(WIDTH, HEIGHT, colorPalette, lightingBands, ambientFactor, maxDepth);
+        Renderer renderer = new(
+            WIDTH,
+            HEIGHT,
+            colorPalette,
+            lightingBands,
+            ambientFactor,
+            maxDepth,
+            useDithering,
+            ditherStrength
+        );
         FrameBuffer buffer = renderer.Render(scene, camera, upScaleFactor, debugMode);
         ImageWriter.WritePPM(outputPPM, buffer);
 

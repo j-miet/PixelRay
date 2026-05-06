83c08013c210be31ea2a15068bc822c379ab62a7
j-miet <191247923+j-miet@users.noreply.github.com>
2026-05-06 18:56:57 +0300

    Rework CLI args parsing

--- PixelRay/Program.cs
+++ PixelRay/Program.cs
@@ -8,79 +8,194 @@ static class CreatePixelRay
 {
     public static void Main(string[] args)
     {
-        // TODO implement better parser which doesn't depend on arg writing order
-        // should separate input and output (-i / -o) so -p flag can be used without needing to save the image
-        if (args.Length >= 3 &&
-            (args[0] == "-i" || args[0] == "--image") &&
-            args[1] != null && args[2] != null)
+        Dictionary<string, string> values = [];
+        values.Add("input", "");
+        values.Add("output", "");
+        values.Add("outputFormat", "");
+        values.Add("preview", "");
+        values.Add("previewSpeed", "0");
+        values.Add("debug", "");
+
+        // parse CLI args and update config dictionary values
+        try
         {
-            var dto = SceneLoader.Load(args[1].ToString());
-            var (scene, camera, settings) = SceneBuilder.Build(dto);
+            string first = args[0];
+            string second = args[1];
 
-            int width = settings.Width;
-            int height = settings.Height;
-            int upScaleFactor = settings.UpScaleFactor;
-
-            // if more options are added, consider passing settings itself and let renderer handle rest
-            Renderer renderer = new(
-                width,
-                height,
-                settings.Palette,
-                settings.LightingBands,
-                settings.MaxBounces,
-                settings.Dithering,
-                settings.DitherLevels,
-                settings.DitherDimension
-            );
-
-            // debug 
-            DebugMode debug = DebugMode.None;
-            if (args.Length >= 5 && args[4] == "--debug" && args[5] != null)
+            if (args[0] != "-i" || args[0] != "--image" || args[1] is not null)
             {
-                string mode = args[5].ToString();
-                debug = mode switch
+                string input = args[1];
+                string format = input.Split(".")[1];
+                if (format == "json")
+                    values["input"] = input;
+                else
                 {
-                    "normals" => DebugMode.Normals,
-                    "distance" => DebugMode.DepthHeat,
-                    "id" => DebugMode.ObjectID,
-                    _ => DebugMode.None
-                };
+                    Console.WriteLine("Scene file must be in json format");
+                    return;
+                }
             }
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Scene file path must be passed first with -i <inputPath>. For example: -i scene.json");
+            return;
+        }
 
-            FrameBuffer buffer = renderer.Render(scene, camera, upScaleFactor, debug);
+        for (int i = 2; i < args.Length; i++)
+        {
+            string val = args[i];
 
-            // output file
-            string outputFile = args[2].ToString();
-            string imageFormat = outputFile.Split(".")[1];
+            switch (val)
+            {
+                case "-o" or "--output":
+                    try
+                    {
+                        if (args[i + 1] is not null)
+                        {
+                            string output = args[i + 1];
+                            string format = output.Split(".")[1];
+                            if (format == "png" || format == "ppm")
+                            {
+                                values["output"] = output;
+                                values["outputFormat"] = format;
+                                i++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Output file must be in json/ppm format");
+                                return;
+                            }
+                        }
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        Console.WriteLine("Usage: -o <outputPath>");
+                        return;
+                    }
+                    break;
 
+                case "-p" or "--preview":
+                    try
+                    {
+                        values["preview"] = "enabled";
+                        string speedVal = args[i + 1];
+
+                        if (speedVal is not null)
+                        {
+                            if (int.TryParse(speedVal, out int intValue))
+                            {
+                                string intToStr = intValue.ToString();
+                                values["previewSpeed"] = intValue >= 0 ? intToStr : "0";
+                                i++;
+                                break;
+                            }
+                            {
+                                Console.WriteLine($"Invalid speed value: {speedVal}");
+                                return;
+                            }
+                        }
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        Console.WriteLine("Usage: -p <value>");
+                        return;
+                    }
+                    break;
+
+                case "--debug":
+                    try
+                    {
+                        if (args[i + 1] is not null)
+                        {
+                            string mode = args[i + 1].ToLower();
+                            ReadOnlySpan<string> modes = ["normals", "distance", "id"];
+                            if (modes.Contains(mode))
+                            {
+                                values["debug"] = mode;
+                                i++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid debug mode flag: use 'normals', 'distance' or 'id'");
+                                return;
+                            }
+                        }
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        Console.WriteLine("Usage: --debug <mode>");
+                        return;
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine($"Unsupported arg: {val}");
+                    break;
+            }
+        }
+
+        if (values["output"] == "" && values["preview"] == "")
+        {
+            Console.WriteLine("Input cannot be called without output/display command, use -o and/or -p flags.");
+            return;
+        }
+
+        // setup
+        var dto = SceneLoader.Load(values["input"]);
+        var (scene, camera, settings) = SceneBuilder.Build(dto);
+
+        int width = settings.Width;
+        int height = settings.Height;
+        int upScaleFactor = settings.UpScaleFactor;
+
+        // if more options are added, consider passing settings itself and let renderer handle rest
+        Renderer renderer = new(
+            width,
+            height,
+            settings.Palette,
+            settings.LightingBands,
+            settings.MaxBounces,
+            settings.Dithering,
+            settings.DitherLevels,
+            settings.DitherDimension
+        );
+
+        DebugMode debug = values["debug"] switch
+        {
+            "normals" => DebugMode.Normals,
+            "distance" => DebugMode.DepthHeat,
+            "id" => DebugMode.ObjectID,
+            _ => DebugMode.None
+        };
+
+        FrameBuffer buffer = renderer.Render(scene, camera, upScaleFactor, debug);
+
+        string outputFile = values["output"];
+        string imageFormat = values["outputFormat"];
+
+        if (values["output"] != "")
+        {
             if (imageFormat == "ppm")
-            {
                 ImageWriter.WritePPM(outputFile, buffer);
-            }
             else if (imageFormat == "png")
-            {
                 ImageWriter.WritePNG(outputFile, buffer);
-            }
             else
-            {
-                throw new Exception("Unsupported image format: use .ppm or .png");
-            }
+                throw new Exception($"Unsupported image format {values["output"]}");
+        }
 
-            // preview image
-            if (args.Length >= 4 && args[3] == "-p")
-            {
-                int speed = 0;
-                if (args.Length == 5 && int.TryParse(args[4], out int value))
-                    speed = value >= 0 ? value : 0;
+        if (values["preview"] == "enabled")
+        {
+            int speed = 0;
+            // parser already does the user input -> int -> str conversion. This is only to convert string -> int again
+            if (int.TryParse(values["previewSpeed"], out int speedValue))
+                speed = speedValue;
 
-                // ensure upscaled resolution has even coordinates, otherwise image display crashes
-                int w = (width % 2 == 0) ? width * upScaleFactor : (width - 1) * upScaleFactor;
-                int h = (height % 2 == 0) ? height * upScaleFactor : (height - 1) * upScaleFactor;
+            // ensure upscaled resolution has even coordinates, otherwise image preview crashes
+            int w = (width % 2 == 0) ? width * upScaleFactor : (width - 1) * upScaleFactor;
+            int h = (height % 2 == 0) ? height * upScaleFactor : (height - 1) * upScaleFactor;
 
-                ImageDisplay display = new(w, h, buffer, speed);
-                display.Display();
-            }
+            ImageDisplay display = new(w, h, buffer, speed);
+            display.Display();
         }
     }
 }
-

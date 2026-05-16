48e2fe867d3b042774cfb615060d8ded34b90f0e
j-miet <191247923+j-miet@users.noreply.github.com>
2026-05-16 19:38:46 +0300

    Update templates to match readme image+animation

--- scene-template/script.lua
+++ scene-template/script.lua
@@ -1,10 +1,25 @@
-torus1 = scene:GetObject("torus1")
+-- objects and lights
+sphere = scene:GetObject("sphere")
+torus = scene:GetObject("torus")
+box = scene:GetObject("box")
+
+pointLight = scene:GetLight("point")
+
+-- camera controls
+frames = 195 -- total frames required ()
+rotationFrames = 120 -- 120 is good for smooth full camera rotation
+unit = 360 / frames; -- split circle into frame units. Then unit*t matches always a full rotation
+
+-- bouncing ball height controls
+frequency = 1.0
+amplitude = 0.2
+decay = 0.3
 
 function update(t)
-    torus1:ResetTransform() -- this resets transform back to default in scene file. Useful for parametrized equations
-
-    frames = 195 -- total frames required ()
-    rotationFrames = 120 -- 120 is good for smooth full camera rotation
+    -- resets transform back to default in scene file. Useful for frame time dependent equations
+    torus:ResetTransform()
+    sphere:ResetTransform()
+    box:ResetTransform()
 
     if t < 15 then
         -- keep camera at default position
@@ -38,9 +53,43 @@ function update(t)
         camera:Position(x, 0, z)
     end
 
-    -- torus y-axis rotation into opposite direction
-    unit = 360 / frames;
-    torus1:Translate(0, -0.05, 0.75)
-    torus1:Rotate(0, 1, 0, -2*unit*t)
-    torus1:Translate(0, 0.05, -0.75)
+    -- box uniform scaling
+    if t < 35 then
+        -- reduce size
+        scale = 1.0-t*0.02
+        box:ScaleUniform(scale)
+    elseif t >= 35 and t < 70 then
+        -- then scale back to original
+        scale = 1.0-(70-t)*0.02
+        box:ScaleUniform(scale)
+    end
+
+    -- bouncing sphere
+    frame = t / 60 -- 60 fps
+    bounces = frame * frequency
+    current_amplitude = amplitude * (decay ^ math.floor(bounces))
+    h = math.abs(math.sin(bounces*math.pi)) * current_amplitude
+    sphere:Translate(0, h, 0)
+
+    -- torus y-axis rotates into opposite direction of camera
+    torus:Rotate(0, 1, 0, -2*unit*t)
+
+    -- pointlight pulsing with yellow color
+    if t > 90 and t < 130 then
+        -- gradually change intensity and color
+        pointLight:Intensity(1.5+(t-90)*0.5)
+        pointLight:Color(
+            0.5 - (t-90)*0.0125,
+            0.5 - (t-90)*0.0125,
+            0.5 + (t-90)*0.0125
+        )
+    elseif t >= 130 and t <= 170 then
+        -- then revert back
+        pointLight:Intensity(1.5+(170-t)*0.5)
+        pointLight:Color(
+            0.5 - (170-t)*0.0125,
+            0.5 - (170-t)*0.0125,
+            0.5 + (170-t)*0.0125
+        )
+    end
 end
